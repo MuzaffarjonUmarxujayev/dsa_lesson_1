127d65fdc03385f286440a4ae04aeb7b3eb124b3
Umarxojayev32456789 <muzaffarumarxojayev86@gmail.com>
2022-04-05 07:12:56 +0500

    Birinchi class

---
+++ bin/dsa_lesson_1.dart
@@ -0,0 +1,7 @@
+import 'package:dsa_lesson_1/dsa_lesson_1.dart' as dsa_lesson_1;
+
+
+void main(List<String> arguments) {
+
+  print('Hello world: ${dsa_lesson_1.calculate()}!');
+}
---
+++ test/dsa_lesson_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:dsa_lesson_1/dsa_lesson_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
